5fc567aac74e0521875ea0f241905cb0ddd8fd53
wql <wql@LAPTOP-95IL3UPS>
2021-01-13 21:23:27 +0800

    1

---
+++ src/main/java/com/wx/watersupplierservice/service/WeixinService.java
@@ -0,0 +1,8 @@
+package com.wx.watersupplierservice.service;
+
+import com.wx.watersupplierservice.po.SysWeixinPo;
+
+public interface WeixinService {
+
+	void saveOrUpdateWx(SysWeixinPo record);
+}
